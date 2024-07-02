f89cefc9bf1184bf6b1a7dca12854337f472fb91
Diksha Jaiswal <dikshajaiswal855@gmail.com>
2024-07-02 16:51:31 +0530

    first new commit

---
+++ src/app/data.js
@@ -0,0 +1,78 @@
+
+export const projectsData = [
+    {
+      id: 1,
+      name: "GlamGait",
+      description: "Buy you best footmates",
+      date: "2024-05-10",
+      demoLink: "https://glamgait-five.vercel.app/",
+    },
+    {
+      id: 2,
+      name: "WebDesigner Portfolio",
+      description: "Including GSAP for microinteractions",
+      date: "2024-04-12",
+      demoLink: "https://dikshajaiswal855.github.io/MYDev1/",
+    },
+    {
+      id: 3,
+      name: "UsabilityHub ",
+      description: "Beautiful Landing Page",
+      date: "2023-10-12",
+      demoLink: "https://dikshajaiswal855.github.io/OIBSIP_task1_landingpage/#",
+    },
+    {
+      id: 4,
+      name: "Temperature Converter",
+      description: "Get you temperature converted to any scale",
+      date: "2022-05-30",
+      demoLink: "https://dikshajaiswal855.github.io/OIBSIP_Task_3_TempConvt/",
+    },
+    {
+      id: 5,
+      name: "Rectangular Box",
+      description: "Move cursor horizontly and see magic",
+      date: "2022-07-12",
+      demoLink: "https://github.com/dikshajaiswal855/rectangularBox",
+    },
+    {
+      id: 6,
+      name: "CollegeLogin",
+      description: "Login site of college having student and staff login",
+      date: "2022-10-01",
+      demoLink: "https://dikshajaiswal855.github.io/collegeLoginPage/",
+    },
+  
+  ];
+  
+  export const BtnList = [
+    { label: "Home", link: "/", icon: "home", newTab: false },
+    { label: "About", link: "/about", icon: "about", newTab: false },
+    { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+    { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+    {
+      label: "Github",
+      link: "https://github.com/dikshajaiswal855",
+      icon: "github",
+      newTab: true,
+    },
+    {
+      label: "LinkedIn",
+      link: "https://www.linkedin.com/in/diksha-jaiswal-b9374a234",
+      icon: "linkedin",
+      newTab: true,
+    },
+    {
+      label: "LeetCode",
+      link: "https://leetcode.com/u/Diksha_Jaiswal/",
+      icon: "leetcode",
+      newTab: true,
+    },
+    {
+      label: "Resume",
+      link: "https://drive.google.com/file/d/1fIfS3LOmtc0J_r5C6-hQX_4JMR1QzuED/view?usp=sharing",
+      icon: "resume",
+      newTab: true,
+    },
+  ];
+  
---
+++ src/app/layout.js
@@ -0,0 +1,39 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import clsx from "clsx";
+import FireFliesBackground from "@/components/FireFliesBackground";
+import Sound from "@/components/Sound";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata = {
+  title: {
+    template:
+      "Diksha_Portfolio",
+    default:
+      "Diksha-Portfolio",
+  },
+  description:
+    "A unique creative portfolio ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={clsx(
+          inter.variable,
+          "bg-background text-foreground font-inter"
+        )}
+      >
+        {children}
+        <FireFliesBackground />
+        <Sound />
+        <div id="my-modal" />
+      </body>
+    </html>
+  );
+}
