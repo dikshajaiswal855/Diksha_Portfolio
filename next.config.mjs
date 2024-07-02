4a6137b7e97bbaa3d131ee7549f374f7c6ae2221
Diksha Jaiswal <dikshajaiswal855@gmail.com>
2024-07-02 19:26:02 +0530

    nextconfig file problem solved

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     swcMinify: true,
+//   };
+  
+//   export default nextConfig;
